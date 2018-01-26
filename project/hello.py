d04ad195315de78132191005899121f81af603bd
Jet Lu <1548036276@qq.com>
2018-01-26 14:05:36 +0800

    python 练习

---
+++ project/hello.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+print 200 + 300
+print 'hello Jet Lu'
+print r'''hello,\n
+world'''
+print len('ABC')
+print u'中文字测试'
+print 'Hi, %s, you have $%d.' % ('Michael', 1000000)
+print 10//3
+print 'hello, {0}, grade up {1:.1f}%'.format('Jet Lu', 25)
+p = ['asp', 'php']
+sq = ['python', 'java', p, 'scheme']
+print 's:',
+print 'a'
